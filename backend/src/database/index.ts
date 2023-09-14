3d32affecc75b2ea0b9f3a60da93679ace15d81e
junaidjaved <JunaidJ.2002@gmail.com>
2023-09-14 05:16:17 -0400

    remove logs

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export default function connectToDatabase() {
     const uri = process.env.MONGODB_CONNECTION_STRING
-    console.log("uri:", uri)
     mongoose
     .connect(uri)
     .then(() => console.log("HealthAid db connection established"))
