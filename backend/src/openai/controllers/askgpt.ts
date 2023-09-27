f173ca7c2e9933503ba857dce6c45c382a836c42
junaidjaved <JunaidJ.2002@gmail.com>
2023-09-27 19:58:39 -0400

    fix

--- backend/src/openai/controllers/askgpt.ts
+++ backend/src/openai/controllers/askgpt.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  // OPENAI_API_KEY variable is missing, idk why
-  apiKey: process.env.OPENAI_API_KEY,
-});
+
 
 export async function AskGPT(req: Request, res: Response) {
   try {
     const prompt = req.body.prompt;
-
+    const openai = new OpenAI({ apiKey: `${process.env.OPENAI_API_KEY}` });
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
       model: "gpt-3.5-turbo",
