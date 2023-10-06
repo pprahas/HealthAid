5e48f2cc97e34beaede242a608822e1f6ec105bb
bysun22 <bysun@purdue.edu>
2023-10-05 21:21:15 -0400

    Added button to doctor onboarding questions

--- frontend/app/(auth)/onboard/components/doctorQuestions.tsx
+++ frontend/app/(auth)/onboard/components/doctorQuestions.tsx
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 import { Checkbox, CheckboxGroup } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import axios, { AxiosError } from "axios";
 
 const DoctorQuestions = () => {
   const questions = [
     {
       question: "What is the name of your clinic",
       placeholder: "HealthAid",
+      key: "name",
+    },
+    {
+      question: "What is your postal code?",
+      placeholder: "01234",
+      key: "postalCode",
+    },
+    {
+      question: "What is the name of your website?",
+      placeholder: "https:your-website.com",
+      key: "website",
+    },
+    {
+      question: "What is your clinic's phone number",
+      placeholder: "+1 (501)-400-5001",
+      key: "phoneNumber",
     },
     {
       question: "What is your speciality (select all that apply)",
@@ -16,25 +34,76 @@ const DoctorQuestions = () => {
         "Dermatology",
         "Infectious Disease",
       ],
+      key: "specialties",
     },
     {
-      question: "What city are you located in?",
+      question: "What is your address?",
       placeholder: "Los Angeles, California",
+      key: "address",
     },
-    {
-      question: "What is your clinic's phone number",
-      placeholder: "+1 (501)-400-5001",
-    },
+    
+  ];
+
+  const clinicInfoKeys = [
+    "name",
+    "postalCode",
+    "website",
+    "phoneNumber",
+    "specialties",
+    "address",
   ];
 
   const [answers, setAnswers] = useState(Array(questions.length).fill(""));
   const [specialities, setSpecialities] = useState([]); // State for selected specialities
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
 
   const handleInputChange = (index: number, value: string | string[]) => {
     const newAnswers = [...answers];
     newAnswers[index] = value;
     setAnswers(newAnswers);
   };
+
+  async function handleRegister() {
+    if (answers.every((answer) => answer !== "")) {
+      try {
+        setLoading(true);
+        let currUserObject = fetchUserData();
+
+        const clinicInfo = {} as { [key: string]: string };
+
+        clinicInfoKeys.forEach((key) => {
+          clinicInfo[key] = answers[key as keyof typeof answers];
+        });
+
+        const postData = {
+          email: currUserObject.email,
+          clinicInfo: clinicInfo,
+        };
+
+        const updateClinicInfoResponse = await axios.post(
+          "http://localhost:8080/doctorClinicInformation",
+          postData
+        );
+        const updateClinicInfo = await updateClinicInfoResponse.data;
+        console.log(updateClinicInfo);
+        setLoading(false);
+        window.location.href = "/home";
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        let errorText = axiosError.response?.data;
+        console.log(errorText);
+        setError("" + errorText);
+      }
+    }
+  }
+
+  function fetchUserData() {
+    let userObjectString = localStorage.getItem("user") ?? "";
+    let userObject = JSON.parse(userObjectString);
+    return userObject;
+  }
 
   return (
     <div className="flex-grow">
@@ -67,6 +136,16 @@ const DoctorQuestions = () => {
             )}
           </div>
         ))}
+        <div className="pt-[3vh]">
+          <Button
+            isLoading={loading}
+            type="button"
+            onClick={handleRegister}
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-2 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Finished!
+          </Button>
+        </div>
       </form>
     </div>
   );
